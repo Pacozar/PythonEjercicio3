34eca37fd7d2fff0347983763304b301eb7fd35b
Francisco Javier Cozar Moreno <pakeraskeras@gmail.com>
2022-02-15 09:40:26 +0100

    Ejercicio3

---
+++ main.py
@@ -0,0 +1,22 @@
+Nombre = 'Manolo'
+Apellidos = 'Fernandez Coto'
+Edad = '23'
+Email = 'mferco@gmail.com'
+Telefono = '656767434'
+Casado = False
+Hijos = False
+Amigos = ['Pedro', 'Juan', 'Pablo', 'Maria', 'Rosa', 'Ana']
+Peliculas = {'2Febrero': 'El rey Leon', '3Febrero': 'Aladdin', '4Febrero': 'La milla verde'}
+
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(Hijos)
+print(Amigos)
+print(Peliculas)
+print(Peliculas['2Febrero'])
+print(Peliculas['3Febrero'])
+print(Peliculas['4Febrero'])
